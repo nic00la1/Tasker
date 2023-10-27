915e472d1420d7752effcd5c5c02aab6dbdc8474
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-27 19:13:33 +0200

    Creating the sections and the title of the Main Page

--- Tasker/MauiProgram.cs
+++ Tasker/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace Tasker
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Regular.ttf", "Roboto");
                 });
 
 #if DEBUG
