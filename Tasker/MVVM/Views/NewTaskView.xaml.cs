5637ce218099ca4159b8141254a692f73e65c3ed
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-28 11:28:23 +0200

    Adding a new category

--- Tasker/MVVM/Views/NewTaskView.xaml.cs
+++ Tasker/MVVM/Views/NewTaskView.xaml.cs
@@ -32,4 +32,30 @@ public partial class NewTaskView : ContentPage
 			await DisplayAlert("Invalid Selection", "You must select a category", "Ok");
 		}
     }
+
+    private async void AddCategoryClicked(object sender, EventArgs e)
+    {
+		var vm = BindingContext as NewTaskViewModel;
+
+		string category =
+			await DisplayPromptAsync("New Category",
+			"Write the new category name",
+			maxLength: 15,
+			keyboard: Keyboard.Text);
+
+		var r = new Random();
+
+		if (!string.IsNullOrEmpty(category))
+		{
+			vm.Categories.Add(new Category
+			{
+				Id = vm.Categories.Max(x => x.Id) + 1,
+				Color = Color.FromRgb(
+					r.Next(0,255),
+					r.Next(0,255),
+					r.Next(00,255)).ToHex(),
+				CategoryName = category
+			});
+		}
+    }
 }
